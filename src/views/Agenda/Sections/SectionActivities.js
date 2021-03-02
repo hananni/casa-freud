4d9f37762394550f5e7ddc4aa01cc5d7af70ad65
natcecilio <tatafly2009@hotmail.com>
2021-03-02 15:58:55 -0300

    att nas pags servicos oferecidos, agenda

--- src/views/Agenda/Sections/SectionActivities.js
+++ src/views/Agenda/Sections/SectionActivities.js
@@ -1,10 +1,10 @@
-import React from "react";
-import { makeStyles } from "@material-ui/core/styles";
-import styles from "assets/jss/material-kit-react/views/componentsSections/basicsStyle.js";
-import { Grid, Box, Typography } from "@material-ui/core";
-import TitleFreud from "components/Typography/TitleFreud";
-import { ACTIVITIES } from "agenda-data";
-import CardActivity from "./CardActivity";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import styles from 'assets/jss/material-kit-react/views/componentsSections/basicsStyle.js';
+import { Box, Typography } from '@material-ui/core';
+import TitleFreud from 'components/Typography/TitleFreud';
+//import { ACTIVITIES } from 'agenda-data';
+//import CardActivity from './CardActivity';
 const useStyles = makeStyles(styles);
 
 const SectionActivities = () => {
@@ -13,17 +13,14 @@ const SectionActivities = () => {
   return (
     <Box pt={5} pb={5}>
       <div className={classes.section}>
-        <div className={classes.container} style={{ display: "grid" }}>
+        <div className={classes.container} style={{ display: 'grid' }}>
           <Box m={4}>
-            <TitleFreud title="Atividades culturais" center />
+            <TitleFreud title="Em breve" center />
           </Box>
           <Box mb={5}>
-            <Typography align="center">
-              É simplesmente uma simulação de texto da indústria tipográfica e
-              de impressos
-            </Typography>
+            <Typography align="center"></Typography>
           </Box>
-          <Grid container spacing={2}>
+          {/*<Grid container spacing={2}>
             {ACTIVITIES.map(activity => {
               return (
                 <Grid item xs={12} key={activity.title}>
@@ -31,7 +28,7 @@ const SectionActivities = () => {
                 </Grid>
               );
             })}
-          </Grid>
+          </Grid>*/}
         </div>
       </div>
     </Box>
--- src/views/ServicosOferecidos/ServicosOferecidos.js
+++ src/views/ServicosOferecidos/ServicosOferecidos.js
@@ -1,17 +1,17 @@
-import React from "react";
-import SectionFeatures from "../../components/SectionFeatures/SectionFeatures";
-import TitlePage from "components/TitlePage/TitlePage";
-import SectionServices from "./Sections/SectionServices";
-import SectionLibrary from "./Sections/SectionLibrary";
-import SectionWorkGroup from "./Sections/SectionWorkGroup";
-import Layout from "views/Layout";
+import React from 'react';
+import SectionFeatures from '../../components/SectionFeatures/SectionFeatures';
+import TitlePage from 'components/TitlePage/TitlePage';
+import SectionServices from './Sections/SectionServices';
+//import SectionLibrary from './Sections/SectionLibrary';
+import SectionWorkGroup from './Sections/SectionWorkGroup';
+import Layout from 'views/Layout';
 
 const ServicosOferecidos = () => {
   return (
     <Layout parallaxContent={<TitlePage title="Serviços oferecidos" />}>
       <SectionFeatures ids={[1, 3, 4]} />
       <SectionServices />
-      <SectionLibrary />
+      {/* <SectionLibrary /> */}
       <SectionWorkGroup />
     </Layout>
   );
